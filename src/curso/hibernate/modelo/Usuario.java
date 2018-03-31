b423a03460491e54a67fcc236b45bf488f0d43e7
Layon Martins <layonmartins03@gmail.com>
2018-03-31 17:17:23 -0300

    Aula 1, 2, 3, 4, 5

--- src/curso/hibernate/modelo/Usuario.java
+++ src/curso/hibernate/modelo/Usuario.java
@@ -1,14 +1,23 @@
 package curso.hibernate.modelo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -24,12 +33,55 @@ public class Usuario {
 	private Date dataNascimento;
 	private int idade;
 	private EstadoCivil estadoCivil;
+	//private Endereco endereco;
+	//private Endereco enderecoComercial;
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	/*
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="logradouro", column=@Column(name="rua"))
+	})
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="logradouro", column=@Column(name="log")),
+		@AttributeOverride(name="cidade", column=@Column(name="cid")),
+		@AttributeOverride(name="numero", column=@Column(name="num"))
+	})
+	public Endereco getEnderecoComercial() {
+		return enderecoComercial;
+	}
+	public void setEnderecoComercial(Endereco enderecoComercial) {
+		this.enderecoComercial = enderecoComercial;
+	}
+	*/
+	
+	
 	
 	@Temporal(TemporalType.DATE)
 	@Column(name = "data_nascimento")
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@JoinTable(name = "usu_endereco", joinColumns = @JoinColumn(name = "id_usuario"))
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+	
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
--- src/curso/hibernate/teste/UsuarioTeste.java
+++ src/curso/hibernate/teste/UsuarioTeste.java
@@ -1,11 +1,13 @@
 package curso.hibernate.teste;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
 
+import curso.hibernate.modelo.Endereco;
 import curso.hibernate.modelo.EstadoCivil;
 import curso.hibernate.modelo.Usuario;
 
@@ -21,13 +23,38 @@ public class UsuarioTeste {
 		usuario.setEstadoCivil(EstadoCivil.SOLTEIRO);
 		usuario.setIdade(25);
 		
+		Endereco endereco = new Endereco();
+		endereco.setCidade("Cidade teste");
+		endereco.setLogradouro("Rual tal");
+		endereco.setNumero(123);
+		usuario.getEnderecos().add(endereco);
+		
+		Endereco enderecoComercial = new Endereco();
+		enderecoComercial.setCidade("Cidade teste C");
+		enderecoComercial.setLogradouro("Rual tal C");
+		enderecoComercial.setNumero(1234);
+		usuario.getEnderecos().add(enderecoComercial);
+		
+		
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
 		session.beginTransaction();
 		
-		session.save(usuario);
+		//session.save(usuario);
 		
 		session.getTransaction().commit();
+		
+		Usuario usuarioBanco = (Usuario) session.get(Usuario.class, 6);
+		
+		
+		
+		
+		System.out.println("Nome: " + usuarioBanco.getNome());
+		System.out.println("Buscou no Banco");
+		List<Endereco> enderecos = usuarioBanco.getEnderecos();
+		System.out.println("Ainda não buscou o banco");
+		System.out.println("Endereço " + enderecos.get(0).getLogradouro());
+		
 		session.close();
 		
 	}
